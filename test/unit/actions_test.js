d9ed543ba26667e526e63333707574608947df35
Joseph Pearson <joseph@inventivelabs.com.au>
2009-02-18 06:24:35 +0800

    Refactored action tests, got end-to-end test passing.
    Signed-off-by: Joshua Peek <josh@joshpeek.com>

--- test/unit/actions_test.js
+++ test/unit/actions_test.js
@@ -1,71 +1,67 @@
 new Test.Unit.Runner({
   setup: function() {
-    runner = this;
-    ed = typeof(ed) == 'object' ? ed : WysiHat.Editor.attach('content');
+    window.ed = window.ed || WysiHat.Editor.attach('content');
   },
 
-  testInvalidActionRegistration: function () {
-    runner.assertRaise(
-      'Error',
-      function () { ed.registerAction({}) }
-    );
-    runner.assertRaise(
-      'Error',
-      function () { ed.registerAction({name: 'xyz'}) }
-    );
-    runner.assertRaise(
-      'Error',
-      function () { ed.registerAction({handler: 'abc'}) }
-    );
-  },
+  testInvalidActionRegistration: function () { with(this) {
+    assertRaise('Error', function () { ed.registerAction({}) });
+    assertRaise('Error', function () { ed.registerAction({name: 'xyz'}) });
+    assertRaise('Error', function () { ed.registerAction({handler: 'abc'}) });
+  }},
 
-  testValidActionRegistration: function () {
-    runner.assertIdentical(
+  testValidActionRegistration: function () { with (this) {
+    assertIdentical(
       ed, 
       ed.registerAction({name: 'xyz', handler: function(){alert('Handled.')}})
     );
-  },
+  }},
 
-  testActionInvocation: function () {
+  testActionInvocation: function () { with (this) {
     ed.registerAction({
       name: 'test1', 
       handler: function (editor) { return "passed"; }
     });
-    runner.assertEqual("passed", ed.invokeAction('test1'));
-  },
+    assertEqual("passed", ed.invokeAction('test1'));
+  }},
 
-  testMultipleArgumentsToHandler: function () {
+  testMultipleArgumentsToHandler: function () { with (this) {
     ed.registerAction({
       name: 'test2',
       handler: function (editor, arg1, arg2) { return arg1 + arg2; }
     });
-    runner.assertEqual(15, ed.invokeAction('test2', 5, 10));
-  },
+    assertEqual(15, ed.invokeAction('test2', 5, 10));
+  }},
 
-  testActionQueryHandler: function () {
+  testActionQueryHandler: function () { with (this) {
     ed.registerAction({
       name: 'test3',
       handler: function (editor) {},
       query: function (editor) { return true; }
     });
-    var pass = false;
-    ed.observe('wysihat:state:test3', function (e) { pass = e.memo.state });
-    runner.assert(!pass);
-    ed.fire('wysihat:cursormove');
-    runner.assert(pass);
-  },
-
-  /* This test FAILS because it runs before the iframe 'load' event is
-   * fired. Is there a way to delay the Runner? */
-  testBoldAction: function () {
-    ed.registerAction(WysiHat.Actions.Bold);
     var state = false;
-    ed.observe('wysihat:state:bold', function (e) { state = e.memo.state });
-    ed.selection.selectNode(ed.down('strong'));
-    runner.assert(state);
-    ed.selection.selectNode(ed.down('em'));
-    runner.assert(!state);
-    ed.invokeAction('bold');
-    runner.assert(state);
-  }
+    ed.observe('wysihat:state:test3', function (e) { state = e.memo.state });
+    assert(!state);
+    ed.fire('wysihat:cursormove');
+    assert(state);
+  }},
+
+  testBoldAction: function () { with (this) {
+    function selectNode(node) {
+      ed.selection.selectNode(node);
+      ed.fire('wysihat:cursormove');
+    }
+
+    wait(1000, function () {
+      ed.registerAction(WysiHat.Actions.Bold);
+      var state = false;
+      var spans = ed.getDocument().getElementsByTagName('span');
+      ed.observe('wysihat:state:bold', function (e) { state = e.memo.state });
+      selectNode(spans[0]);
+      assert(state);
+      selectNode(spans[1]);
+      assert(!state);
+      ed.invokeAction('bold');
+      assert(state);
+    });
+  }}
 });
