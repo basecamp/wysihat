f377828547ecc0fba4b65e60447f13e8c94e1436
Joshua Peek <josh@joshpeek.com>
2009-02-27 20:24:59 -0600

    update copyright years

--- src/wysihat.js
+++ src/wysihat.js
@@ -1,5 +1,5 @@
 /*  WysiHat - WYSIWYG JavaScript framework, version 0.1
- *  (c) 2008 Joshua Peek
+ *  (c) 2008-2009 Joshua Peek
  *
  *  WysiHat is freely distributable under the terms of an MIT-style license.
  *--------------------------------------------------------------------------*/
