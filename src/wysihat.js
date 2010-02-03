2f7761f740a3992ca7a94eb0a62924ef2cd0a821
Joshua Peek <josh@joshpeek.com>
2010-02-03 14:32:52 -0600

    0.2.1 release

--- src/wysihat.js
+++ src/wysihat.js
@@ -1,5 +1,5 @@
-/*  WysiHat - WYSIWYG JavaScript framework, version 0.2
- *  (c) 2008-2009 Joshua Peek
+/*  WysiHat - WYSIWYG JavaScript framework, version 0.2.1
+ *  (c) 2008-2010 Joshua Peek
  *
  *  WysiHat is freely distributable under the terms of an MIT-style license.
  *--------------------------------------------------------------------------*/
