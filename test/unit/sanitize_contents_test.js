43a41dc6f431959b9720de39bb26f88f29f3c072
Joshua Peek <josh@joshpeek.com>
2010-05-24 21:25:41 -0500

    fix tests on ie

--- test/unit/sanitize_contents_test.js
+++ test/unit/sanitize_contents_test.js
@@ -1,5 +1,5 @@
 function sanitize(html, options) {
-  return Element("div").update(html).sanitizeContents(options).innerHTML;
+  return new Element("div").update(html).sanitizeContents(options).innerHTML.toLowerCase();
 }
 
 new Test.Unit.Runner({
@@ -7,70 +7,70 @@ new Test.Unit.Runner({
     var runner = this;
 
     runner.assertEqual(
-      "Hello",
-      sanitize("Hello", {})
+      "hello",
+      sanitize("hello", {})
     );
 
     runner.assertEqual(
-      "Hello",
-      sanitize("<strong>Hello</strong>", {})
+      "hello",
+      sanitize("<strong>hello</strong>", {})
     );
 
     runner.assertEqual(
-      "Hello",
-      sanitize("<em><strong>Hello</strong></em>", {})
+      "hello",
+      sanitize("<em><strong>hello</strong></em>", {})
     );
 
     runner.assertEqual(
-      "<strong>Hello</strong>",
-      sanitize("<strong>Hello</strong>", {allow: "strong"})
+      "<strong>hello</strong>",
+      sanitize("<strong>hello</strong>", {allow: "strong"})
     );
 
     runner.assertEqual(
-      "<strong>Hello</strong>",
-      sanitize("<em><strong>Hello</strong></em>", {allow: "strong"})
+      "<strong>hello</strong>",
+      sanitize("<em><strong>hello</strong></em>", {allow: "strong"})
     );
 
     runner.assertEqual(
-      "<strong>Hello</strong>",
-      sanitize("<strong><em>Hello</em></strong>", {allow: "strong"})
+      "<strong>hello</strong>",
+      sanitize("<strong><em>hello</em></strong>", {allow: "strong"})
     );
 
     runner.assertEqual(
-      "<strong>Hello</strong> World!",
-      sanitize("<p><strong><em>Hello</em></strong> World!</p>", {allow: "strong"})
+      "<strong>hello</strong> world!",
+      sanitize("<p><strong><em>hello</em></strong> world!</p>", {allow: "strong"})
     );
 
     runner.assertEqual(
-      "<strong><em>Hello</em></strong>",
-      sanitize("<strong><em>Hello</em></strong>", {allow: "em, strong"})
+      "<strong><em>hello</em></strong>",
+      sanitize("<strong><em>hello</em></strong>", {allow: "em, strong"})
     );
 
     runner.assertEqual(
-      "<a>Google</a>",
-      sanitize("<a href='http://www.google.com/' title=\"Google\">Google</a>", {allow: "a"})
+      "<a>google</a>",
+      sanitize("<a href='http://www.google.com/' title=\"google\">google</a>", {allow: "a"})
     );
 
     runner.assertEqual(
-      "<a href=\"http://www.google.com/\">Google</a>",
-      sanitize("<a href=\"http://www.google.com/\" title=\"Google\">Google</a>", {allow: "a[href]"})
+      "<a href=\"http://www.google.com/\">google</a>",
+      sanitize("<a href=\"http://www.google.com/\" title=\"google\">google</a>", {allow: "a[href]"})
     );
 
     if (Prototype.Browser.IE) {
       runner.assertEqual(
-        "Hello<span id=bookmark> </span>",
-        sanitize("Hello<span id=bookmark> </span>", {allow: "span[id]"})
+        "hello<span id=bookmark> </span>",
+        sanitize("hello<span id=bookmark> </span>", {allow: "span[id]"})
       );
     } else {
       runner.assertEqual(
-        "Hello<span id=\"bookmark\"> </span>",
-        sanitize("Hello<span id=\"bookmark\"> </span>", {allow: "span[id]"})
+        "hello<span id=\"bookmark\"> </span>",
+        sanitize("hello<span id=\"bookmark\"> </span>", {allow: "span[id]"})
       );
     }
 
     runner.assertEqual(
-      "<img src=\"http://www.google.com/intl/en_ALL/images/logo.gif\">",
-      sanitize("<img src=\"http://www.google.com/intl/en_ALL/images/logo.gif\">", {allow: "img[src], a[href]"})
+      "<img src=\"http://www.google.com/intl/en_all/images/logo.gif\">",
+      sanitize("<img src=\"http://www.google.com/intl/en_all/images/logo.gif\">", {allow: "img[src], a[href]"})
     );
   }
 });
